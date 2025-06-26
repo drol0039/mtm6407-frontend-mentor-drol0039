3932c5dd67b0ac4eef922c671e5ffb04b79df1ee
Rie Drolet <drol0039@algonquinlive.com>
2025-06-25 22:11:27 -0400

    almost grid!

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+const hamburger = document.getElementById('hamburger');
+const navMenu = document.getElementById('nav-menu');
+const closeBtn = document.getElementById('close');
 
+hamburger.addEventListener('click', () => {
+  navMenu.classList.add('active');
+});
+
+closeBtn.addEventListener('click', () => {
+  navMenu.classList.remove('active');
+});
